35e588f05195d8cafb9686b3e3eb649386e8ecda
iulus <irfanulus34@gmail.com>
2024-03-28 16:38:29 +0300

    V1

---
+++ src/test/java/Jenkinnnnns.java
@@ -0,0 +1,45 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class Jenkinnnnns {
+
+    @Test
+    void Test1(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test2(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test3(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test4(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test5(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test6(){
+        assertTrue(true);
+    }
+
+    @Test
+    void Test7(){
+        assertTrue(true);
+    }
+
+
+
+
+}
